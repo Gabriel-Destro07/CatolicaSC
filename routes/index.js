c9a662e913177fd8cf807975d757d50225d0bc1f
Gabriel Destro <gabriel.destro@catolicasc.edu.br>
2026-05-25 13:06:53 -0300

    Atualizar estrutura do projeto e melhorias na interface

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const authMiddleware = require('../middlewares/auth'); // Middleware para proteger rotas que exigem autenticação
+const upload = require('../middlewares/multer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,10 +22,19 @@ router.post('/login', userController.login);
 // Rota para processar o logout
 router.get('/logout', userController.logout);
 
-// Rota para exibir o feed de vídeos (protegida por autenticação com middleware)
-router.get('/feed', authMiddleware, (req, res) => { // middleware estre a rota feed e a função de callback
-   res.render('home', { user: req.session.user }); // Passa os dados do usuário para a view, se necessário
+// Rota para exibir o feed de vídeos (protegida por autenticação)
+router.get('/feed', authMiddleware, async (req, res) => {
+    const user = await userController.getProfile(req.session.user.id);
+    res.render('home', { user });
 });
 
+// Rota para exibir o perfil do usuário (protegida por autenticação)
+router.get('/profile/edit', authMiddleware, async (req, res) => {
+    const user = await userController.getProfile(req.session.user.id);
+    res.render('edit-profile', { user });
+});
+
+// Rota de atualização (Protegida + Upload de 1 arquivo chamado 'profilePicture')
+router.post('/profile/edit', authMiddleware, upload.single('profilePicture'), userController.updateProfile);
 
 module.exports = router;
